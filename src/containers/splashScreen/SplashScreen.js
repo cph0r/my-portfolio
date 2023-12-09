0095bff9712c0a158d6a6c486ee6e646b81552a1
chiragphor <chiragphor19@gmail.com>
2023-12-09 16:17:31 +0530

    added animation

--- src/containers/splashScreen/SplashScreen.js
+++ src/containers/splashScreen/SplashScreen.js
@@ -1,19 +1,23 @@
 import React, {useContext} from "react";
 import "./SplashScreen.css";
-import DisplayLottie from "../../components/displayLottie/DisplayLottie";
-import {greeting, splashScreen} from "../../portfolio";
+// import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import {greeting} from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
+import AnimatedSVG from "./Animate";
 
 export default function SplashScreen() {
   const {isDark} = useContext(StyleContext);
   return (
     <div className={isDark ? "dark-mode splash-container" : "splash-container"}>
       <div className="splash-animation-container">
-        <DisplayLottie animationData={splashScreen.animation} />
+          <AnimatedSVG></AnimatedSVG>
+
+
+        {/*<DisplayLottie animationData={splashScreen.animation} />*/}
       </div>
       <div className="splash-title-container">
         <span className="grey-color"> &lt;</span>
-        <span className="splash-title">{greeting.username}</span>
+        {/*<span className="splash-title">{greeting.username}</span>*/}
         <span className="grey-color">/&gt;</span>
       </div>
     </div>
